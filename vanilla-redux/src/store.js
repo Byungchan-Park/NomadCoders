c28010db9471888690a4df66b7dc76e231738c43
Byungchan-Park <pbc9236@gmail.com>
2020-09-18 16:19:32 +0900

    code refactoring with createSlice function of Redux Tool Kit

--- vanilla-redux/src/store.js
+++ vanilla-redux/src/store.js
@@ -1,12 +1,12 @@
-import { configureStore, createAction, createReducer } from "@reduxjs/toolkit"
+import { configureStore, createSlice } from "@reduxjs/toolkit"
 
 // createAction
-const addToDo = createAction("ADD")
-const deleteToDo = createAction("DELETE")
-console.log(addToDo.type) // "ADD"
-console.log(deleteToDo.type) // "DELETE"
-console.log(addToDo()) // {type:"ADD", payload: undefined}
-console.log(deleteToDo()) // {type:"DELETE", payload: undefined}
+// const addToDo = createAction("ADD")
+// const deleteToDo = createAction("DELETE")
+// console.log(addToDo.type) // "ADD"
+// console.log(deleteToDo.type) // "DELETE"
+// console.log(addToDo()) // {type:"ADD", payload: undefined}
+// console.log(deleteToDo()) // {type:"DELETE", payload: undefined}
 
 // define initial State of Redux store
 // when page is refreshed, then refer the data which is stored in the localStorage.
@@ -18,22 +18,30 @@ const defaultState = JSON.parse(localStorage.getItem("toDos")) || []
 // when you mutate something, you should not return.
 // second option : returns an new state
 // when you return something, it should be a new state here.
-// ? Redux and Immer works.
-const reducer = createReducer(defaultState, {
-  [addToDo]: (state, action) => {
-    state.push({ text: action.payload, id: Date.now() })
-  },
-  [deleteToDo]: (state, action) => {
-    return state.filter((toDo) => toDo.id !== action.payload)
+// ? Redux Developer kit works with Immer behind the scenes.
+// const reducer = createReducer(defaultState, {
+//   [addToDo]: (state, action) => {
+//     state.push({ text: action.payload, id: Date.now() })
+//   },
+//   [deleteToDo]: (state, action) => {
+//     return state.filter((toDo) => toDo.id !== action.payload)
+//   },
+// })
+
+const toDos = createSlice({
+  name: "toDosReducer",
+  initialState: defaultState,
+  reducers: {
+    add: (state, action) => {
+      state.push({ text: action.payload, id: Date.now() })
+    },
+    remove: (state, action) => {
+      return state.filter((toDo) => toDo.id !== action.payload)
+    },
   },
 })
 
+export const { add, remove } = toDos.actions
+
 // configureStore basically allow user use Redux Developer Tools.
-const store = configureStore({ reducer })
-
-export const actionCreators = {
-  addToDo,
-  deleteToDo,
-}
-
-export default store
+export default configureStore({ reducer: toDos.reducer })
