caac72e7a4868d4efa012b8b426536eff36fb047
Byungchan-Park <pbc9236@gmail.com>
2020-09-20 20:50:43 +0900

    data-fetching using axios to get movies data

--- movie_app_2020/src/App.js
+++ movie_app_2020/src/App.js
@@ -1,14 +1,29 @@
 import React, { useEffect, useState } from "react"
-import PropTypes from "prop-types"
+import axios from "axios"
+import Movie from "./Movie"
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true)
   const [movies, setMovies] = useState([])
+
+  const getMovies = async () => {
+    const {
+      data: { movies },
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating").then((response) => response.data)
+    console.log(movies)
+    setMovies(movies)
+    setIsLoading(false)
+  }
+
   useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false)
-    }, 4000)
+    getMovies()
   }, [])
-  return <div>{isLoading ? "Loading..." : "We are ready"}</div>
+  return (
+    <div>
+      {isLoading
+        ? "Loading..."
+        : movies.map((movie) => <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />)}
+    </div>
+  )
 }
 export default App
